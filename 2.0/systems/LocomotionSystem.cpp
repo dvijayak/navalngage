bb0f3f527ce06b4e9d7dcb87c25ffb9bc3636dd4
Daniel Vijayakumar <daniel.vkumar@gmail.com>
2016-10-22 18:58:04 -0400

    Made some progress on Locomotion.

--- 2.0/systems/LocomotionSystem.cpp
+++ 2.0/systems/LocomotionSystem.cpp
@@ -3,6 +3,9 @@
 #include "global.hpp"
 #include "GameObjectFactory.hpp"
 
+#include "PositionComponent.hpp"
+#include "MovementComponent.hpp"
+
 LocomotionSystem::LocomotionSystem ()
 {}
 
@@ -11,8 +14,22 @@ LocomotionSystem::~LocomotionSystem ()
 
 void LocomotionSystem::Update (size_t dt, GameObjectFactory const& factory)
 {
-	// for (auto& pGo : factory.GetAllGameObjects())
-	// {
-		// console("Running the locomotion system by " << dt << " ms.");
-	// }
+	auto filter = [] (GameObject const& go) { return 
+		go.HasComponent<PositionComponent>() &&
+		go.HasComponent<MovementComponent>();
+	};
+	for (auto& pGo : factory.ResolveObjects(filter))
+	{
+		// TODO: Mass affects Rotation
+		// TODO: Mass affects Movement
+		VectorF& velMod = pGo->GetComponent<MovementComponent>()->GetVelocityMod();
+		// TODO: Rotation affects Movement
+
+		// Movement (velocity) affects Position
+		// We know velocity and elapsed time, we need to find displacement
+		// d = v * t
+		// Add position and displacement vectors to get the new position
+		VectorF& posMod = pGo->GetComponent<PositionComponent>()->GetPositionMod();
+		posMod += velMod * (dt/1000.0); // dt is in ms, v is in m/s
+	}
 }
